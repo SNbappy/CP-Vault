bf6b57c6d62900697abb37a2f05286ca0f0b3ca1
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2025-03-13 22:35:45 +0600

    updated

---
+++ learing-topics/Random_Things/newbieToPupil.cpp
@@ -0,0 +1,10 @@
+/*************
+Newbie To Pupil:
+****************
+A: ---> (800rated) C++, STL ---> 100
+900 rating
+B: ---> (1000 - 1200 rated) ---> Prefix sum, binary Search,Bit Manipulation
+                                To Pointer, Sliding window, Number Theory
+Participate in More and more contest
+
+*/
