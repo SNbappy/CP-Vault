6960ed9474a8d5c1516b63392a2bd9e0d8e551e0
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2025-05-13 21:23:18 +0600

    updated

--- DFS.cpp
+++ DFS.cpp
@@ -1,22 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 const int N = 1e5 + 10;
-
 vector<int> adj[N];
-bool vis[N];
+bool visited[N];
 
-void dfs(int vertex){
-    
+void dfs(int node)
+{
+    visited[node] = true;
+    cout << node << " ";
+    for (int neighbour : adj[node])
+    {
+        if (!visited[neighbour])
+            dfs(neighbour);
+    }
 }
 
-int main(){
+int main()
+{
     int n, m;
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < m; i++)
     {
         int u, v;
         cin >> u >> v;
-
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    
+    dfs(1);
 }
