4a3109c406496db8184e0f5e522eb286cc077074
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2025-07-23 21:05:06 +0600

    Accepted

---
+++ B_Left_and_Down.cpp
@@ -0,0 +1,81 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+
+Author: Depressed_C0der
+Created: 2025-07-22 20:44:27
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+#define ll long long
+#define tst   \
+    int t;    \
+    cin >> t; \
+    while (t--)
+#define el '\n'
+#define yes cout << "YES" << el
+#define no cout << "NO" << el
+#define Depressed_C0der int32_t main()
+#define Goodbye return 0;
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+const int MOD = 1e9 + 7;
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+/// change int to any data type
+/// less_equal for multiset increasing order
+/// less for set increasing order
+/// greater_equal for multiset decreasing order
+/// greater for set decreasing order
+
+/// cout<<*X.find_by_order(1)<<endl; // iterator to the k-th largest element
+/// cout<<X.order_of_key(-5)<<endl;  // number of items in a set that are strictly smaller than our item
+
+/************************************************************
+ *                   Utility Function                      *
+ ************************************************************/
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+void Beche_achi()
+{
+    ll a, b, k;
+    cin >> a >> b >> k;
+    ll x = gcd(a, b);
+    if(a%x == 0 and a / x <= k and b%x == 0 and b / x <= k){
+        cout << 1 << el;
+        return;
+    }
+    if(k >= a and k >= b){
+        cout << 1 << el;
+        return;
+    }
+    cout << 2 << el;
+}
+
+/************************************************************
+ *                      Main Function                      *
+ ************************************************************/
+
+Depressed_C0der
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    tst
+    Beche_achi();
+
+    Goodbye
+}
