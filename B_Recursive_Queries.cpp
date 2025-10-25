9e23cc7a45e8c7cddc72909ebed9313e333d1c01
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2025-10-25 16:20:47 +0600

    Accepted

---
+++ B_Recursive_Queries.cpp
@@ -0,0 +1,37 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2025-10-25 16:20:09
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Solve() {
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++)
+    {
+        int l, r, k;
+        cin >> l >> r >> k;
+    }
+    
+}
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++) {
+        // Cout << \"Case \" << i << \": \";
+        Solve();
+    }
+    return 0;
+}
