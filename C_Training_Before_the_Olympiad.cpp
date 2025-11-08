a273758f5a7d85e2e07788e5e3d2d63847a1f951
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2025-11-08 19:40:16 +0600

    Accepted

---
+++ C_Training_Before_the_Olympiad.cpp
@@ -0,0 +1,60 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2025-11-08 18:44:31
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Solve()
+{
+    int n;
+    cin >> n;
+    int sum = 0, odd = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        sum += a;
+        odd += a % 2;
+        int res;
+        if (odd == 1 and i == 0)
+        {
+            res = 0;
+        }
+        else
+        {
+            res = odd / 3;
+            if (odd % 3 == 1)
+            {
+                res += 1;
+            }
+        }
+        cout << sum - res << " ";
+    }
+    cout << '\n';
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // Cout << \"Case \" << i << \": \";
+        Solve();
+    }
+    return 0;
+}
