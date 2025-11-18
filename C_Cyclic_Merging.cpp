d2bbe79915c4c1597acbcb0fd65e67461e8fdda9
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2025-11-18 20:13:15 +0600

    Accepted

--- C_Cyclic_Merging.cpp
+++ C_Cyclic_Merging.cpp
@@ -1,65 +1,47 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
 Author: Depressed_C0der
-Created: 2025-11-16 21:25:15
+Created: 2025-11-17 17:37:21
 */
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
 #define all(n) n.begin(), n.end()
 #ifndef ONLINE_JUDGE
-#define debug(...)                                                  \
-    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
-    _print(__VA_ARGS__)
+#define debug(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
 #else
 #define debug(...)
 #endif
 
-void Solve()
-{
+void Solve() {
     int n;
     cin >> n;
-    deque<int> a(n);
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    int sum_edges = 0;
+    int max_edge = 0;
+
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        int x = a[i];
+        int y = a[(i + 1) % n];
+        int w = max(x, y);
+        sum_edges += w;
+        max_edge = max(max_edge, w);
     }
 
-    int cost = 0;
-
-    int i = 0, j = 1, l = n - 1;
-    while (a.size() >= 2)
-    {
-        int x;
-        if (a[1] < a[a.size() - 1])
-        {
-            x = max(a[0], a[1]);
-            cost += x;
-            a.pop_front();
-            a[0] = x;
-        }
-        else
-        {
-            x = max(a[0], a[a.size() - 1]);
-            cost += x;
-            a.pop_back();
-            a[0] = x;
-        }
-        cerr << x << '\n';
-    }
-    cout << cost << '\n';
+    cout << (sum_edges - max_edge) << "\n";
 }
 
-signed main()
-{
+signed main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(0); cout.tie(0);
     int tc = 1;
     cin >> tc;
 
-    for (int i = 1; i <= tc; i++)
-    {
+    for (int i = 1; i <= tc; i++) {
         // Cout << \"Case \" << i << \": \";
         Solve();
     }
---
+++ Creating_Strings.cpp
@@ -0,0 +1,47 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2025-11-18 19:49:17
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Solve()
+{
+    string s;
+    cin >> s;
+    sort(all(s));
+    set<string>
+        x;
+    do{
+        x.insert(s);
+    } while (next_permutation(all(s)));
+    cout << x.size() << '\n';
+    for (auto y : x)
+        cout
+            << y << '\n';
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // Cout << \"Case \" << i << \": \";
+        Solve();
+    }
+    return 0;
+}
