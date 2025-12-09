99154f2e204d2a6a892c139c050a6fc89d0599ed
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2025-12-09 18:42:49 +0600

    Accepted

--- B_XOR_Array.cpp
+++ B_XOR_Array.cpp
@@ -8,23 +8,52 @@ using namespace std;
 #define int long long
 #define all(n) n.begin(), n.end()
 #ifndef ONLINE_JUDGE
-#define debug(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
 #else
 #define debug(...)
 #endif
 
-void Solve() {
-    int n, l, r;
-    cin >> n >> l >> r;
-    
+void Solve()
+{
+    int n;
+    cin >> n;
+    int l, r;
+    cin >> l >> r;
+    --l, --r;
+    int c = 1, x = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == r)
+            cout << x << ' ';
+        else
+        {
+            int a = c, b = 1;
+            while (a % 2 == 0)
+                b *= 2, a /= 2;
+            if (i == r + 1)
+                cout << (b ^ x) << ' ';
+            else
+                cout << b << ' ';
+            if (i >= l && i <= r)
+                x = (x ^ b);
+            c++;
+        }
+    }
+    cout << '\n';
 }
-signed main() {
+
+signed main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
     int tc = 1;
     cin >> tc;
 
-    for (int i = 1; i <= tc; i++) {
+    for (int i = 1; i <= tc; i++)
+    {
         // Cout << \"Case \" << i << \": \";
         Solve();
     }
