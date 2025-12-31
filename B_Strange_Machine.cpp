d1d883031e5822906432b10b07e6af6055412611
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2025-12-31 12:00:10 +0600

    Accepted

--- B_Strange_Machine.cpp
+++ B_Strange_Machine.cpp
@@ -1,30 +1,59 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
 Author: Depressed_C0der
-Created: 2025-10-24 20:48:10
+Created: 2025-12-31 11:33:58
 */
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
 #define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
 #ifndef ONLINE_JUDGE
-#define debug(...) cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__)
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
 #else
 #define debug(...)
 #endif
 
-void Solve() {
+void Depressed_C0der()
+{
+    int n, q;
+    string s;
+    cin >> n >> q >> s;
+    vector<int> a(q);
+    for (int i = 0; i < q; i++)
+    {
+        cin >> a[i];
+    }
     
 }
-signed main() {
+
+signed main()
+{
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
+    cout.tie(0);
+
     int tc = 1;
     cin >> tc;
 
-    for (int i = 1; i <= tc; i++) {
-        // Cout << \"Case \" << i << \": \";
-        Solve();
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
     }
     return 0;
 }
