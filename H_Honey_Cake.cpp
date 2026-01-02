75308659a5fed0e689c4b44f24e5480cfa57b20b
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-01-02 10:30:18 +0600

    Accepted

---
+++ H_Honey_Cake.cpp
@@ -0,0 +1,62 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-01-01 22:35:14
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int w, h, d, n;
+    cin >> w >> h >> d >> n;
+    int a = gcd(n, w);
+    n /= a;
+    int b = gcd(n, h);
+    n /= b;
+    int c = gcd(n, d);
+    n /= c;
+    if (n == 1)
+        cout << a - 1 << " " << b - 1 << " " << c - 1;
+    else
+        cout << -1 << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    // cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
