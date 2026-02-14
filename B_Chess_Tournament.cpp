6b4228ea1d81b47a39f337925c396166c557dde5
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-02-15 00:39:00 +0600

    Accepted

--- B_Chess_Tournament.cpp
+++ B_Chess_Tournament.cpp
@@ -1,110 +1,51 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
-
 Author: Depressed_C0der
-Created: 2025-02-27 06:29:40
+Created: 2026-02-15 00:38:46
 */
-
 #include <bits/stdc++.h>
 using namespace std;
-
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-
-#define ll long long
-#define tst   \
-    int t;    \
-    cin >> t; \
-    while (t--)
-#define el '\n'
-#define yes cout << "YES" << el
-#define no cout << "NO" << el
-#define Depressed_C0der int32_t main()
-#define Goodbye return 0;
+#define int long long
 #define all(n) n.begin(), n.end()
 #define rall(n) n.rbegin(), n.rend()
 #define pb push_back
-const int MOD = 1e9 + 7;
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
 
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-/// change int to any data type
-/// less_equal for multiset increasing order
-/// less for set increasing order
-/// greater_equal for multiset decreasing order
-/// greater for set decreasing order
-
-/// cout<<*X.find_by_order(1)<<endl; // iterator to the k-th largest element
-/// cout<<X.order_of_key(-5)<<endl;  // number of items in a set that are strictly smaller than our item
-
-/************************************************************
- *                   Utility Function                      *
- ************************************************************/
-
-int gcd(int a, int b)
+void Depressed_C0der()
 {
-    if (b == 0)
-        return a;
-    else
-        return gcd(b, a % b);
+    
 }
 
-void Beche_achi()
-{
-    ll n;
-    cin >> n;
-    string s;
-    cin >> s;
-    ll one = 0, two = 0;
-
-    for (ll i = 0; i < n; i++)
-    {
-        if (s[i] == '1')
-            one++;
-        else
-            two++;
-    }
-
-    if (two == 1)
-    {
-        no;
-        return;
-    }
-
-    yes;
-    vector<string> grid(n, string(n, '='));
-
-    for (ll i = 0; i < n; i++)
-        grid[i][i] = 'X';
-
-    vector<int> two_indices;
-    for (ll i = 0; i < n; i++)
-        if (s[i] == '2')
-            two_indices.push_back(i);
-
-    ll size = two_indices.size();
-    for (ll i = 0; i < size; i++)
-    {
-        ll x = two_indices[i], y = two_indices[(i + 1) % size];
-        grid[x][y] = '+';
-        grid[y][x] = '-';
-    }
-
-    for (ll i = 0; i < n; i++)
-        cout << grid[i] << el;
-}
-
-/************************************************************
- *                      Main Function                      *
- ************************************************************/
-
-Depressed_C0der
+signed main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    tst
-    Beche_achi();
+    int tc = 1;
+    cin >> tc;
 
-    Goodbye
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
 }
--- B_Triangles_on_a_Rectangle.cpp
+++ B_Triangles_on_a_Rectangle.cpp
@@ -1,72 +1,104 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
-
 Author: Depressed_C0der
-Created: 2024-12-27 16:47:44
+Created: 2026-02-15 00:01:52
 */
-
 #include <bits/stdc++.h>
 using namespace std;
-
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-
-#define ll long long
-#define tst   \
-    int t;    \
-    cin >> t; \
-    while (t--)
-#define el '\n'
-#define yes cout << "YES" << el
-#define no cout << "NO" << el
-#define Depressed_C0der int32_t main()
-#define Goodbye return 0;
+#define int long long
 #define all(n) n.begin(), n.end()
 #define rall(n) n.rbegin(), n.rend()
 #define pb push_back
-const int MOD = 1e9 + 7;
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
 
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-/// change int to any data type
-/// less_equal for multiset increasing order
-/// less for set increasing order
-/// greater_equal for multiset decreasing order
-/// greater for set decreasing order
-
-/// cout<<*X.find_by_order(1)<<endl; // iterator to the k-th largest element
-/// cout<<X.order_of_key(-5)<<endl;  // number of items in a set that are strictly smaller than our item
-
-/************************************************************
- *                   Utility Function                      *
- ************************************************************/
-
-int gcd(int a, int b)
+void Depressed_C0der()
 {
-    if (b == 0)
-        return a;
-    else
-        return gcd(b, a % b);
-}
-
-void Beche_achi()
-{
-    ll w, h;
+    int w, h;
     cin >> w >> h;
-    
+    int kh0;
+    cin >> kh0;
+    vector<int> x0(kh0);
+    for (int i = 0; i < kh0; i++)
+    {
+        cin >> x0[i];
+    }
+
+    int maxh0 = *max_element(all(x0));
+    int minh0 = *min_element(all(x0));
+
+    int kh1;
+    cin >> kh1;
+    vector<int> x1(kh1);
+    for (int i = 0; i < kh1; i++)
+    {
+        cin >> x1[i];
+    }
+
+    int maxh1 = *max_element(all(x1));
+    int minh1 = *min_element(all(x1));
+
+    int kv0;
+    cin >> kv0;
+    vector<int> y0(kv0);
+    for (int i = 0; i < kv0; i++)
+    {
+        cin >> y0[i];
+    }
+
+    int maxv0 = *max_element(all(y0));
+    int minv0 = *min_element(all(y0));
+
+    int kv1;
+    cin >> kv1;
+    vector<int> y1(kv1);
+    for (int i = 0; i < kv1; i++)
+    {
+        cin >> y1[i];
+    }
+
+    int maxv1 = *max_element(all(y1));
+    int minv1 = *min_element(all(y1));
+
+    int ans1 = abs(maxh0 - minh0) * h;
+    int ans2 = abs(maxh1 - minh1) * h;
+    int ans3 = abs(maxv0 - minv0) * w;
+    int ans4 = abs(maxv1 - minv1) * w;
+    ans1 = max(ans1, ans2);
+    ans1 = max(ans1, ans3);
+    ans1 = max(ans1, ans4);
+    cout << ans1 << "\n";
 }
 
-/************************************************************
- *                      Main Function                      *
- ************************************************************/
-
-Depressed_C0der
+signed main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    //tst
-    Beche_achi();
+    int tc = 1;
+    cin >> tc;
 
-    Goodbye
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
 }
