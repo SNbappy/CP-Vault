58b21f07fd2d9c48d7132a630b879e3af2c4eba8
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-02-19 10:53:33 +0600

    Accepted

--- A_Road_To_Zero.cpp
+++ A_Road_To_Zero.cpp
@@ -1,72 +1,91 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
-
 Author: Depressed_C0der
-Created: 2025-03-08 14:36:29
+Created: 2026-02-19 10:40:40
 */
-
 #include <bits/stdc++.h>
 using namespace std;
-
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-
-#define ll long long
-#define tst   \
-    int t;    \
-    cin >> t; \
-    while (t--)
-#define el '\n'
-#define yes cout << "YES" << el
-#define no cout << "NO" << el
-#define Depressed_C0der int32_t main()
-#define Goodbye return 0;
+#define int long long
 #define all(n) n.begin(), n.end()
 #define rall(n) n.rbegin(), n.rend()
 #define pb push_back
-const int MOD = 1e9 + 7;
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
 
-typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-/// change int to any data type
-/// less_equal for multiset increasing order
-/// less for set increasing order
-/// greater_equal for multiset decreasing order
-/// greater for set decreasing order
-
-/// cout<<*X.find_by_order(1)<<endl; // iterator to the k-th largest element
-/// cout<<X.order_of_key(-5)<<endl;  // number of items in a set that are strictly smaller than our item
-
-/************************************************************
- *                   Utility Function                      *
- ************************************************************/
-
-int gcd(int a, int b)
+void Depressed_C0der()
 {
-    if (b == 0)
-        return a;
-    else
-        return gcd(b, a % b);
-}
-
-void Beche_achi()
-{
-    ll x, y, a, b;
+    int x, y, a, b;
     cin >> x >> y >> a >> b;
-    
+    if(a*2 > b){
+        int ans = 0;
+        if(x < y){
+            y -= x;
+            
+            ans += (b * x);
+            x = 0;
+        }
+        else{
+            x -= y;
+            ans += (b * y);
+            y = 0;
+        }
+        if (x)
+        {
+            // x--;
+            ans += a*x;
+        }
+        if (y)
+        {
+            // y--;
+            ans += a*y;
+        }
+        cout << ans << "\n";
+    }
+    else{
+        int ans = 0;
+        if (x)
+        {
+            // x--;
+            ans += a * x;
+        }
+        if (y)
+        {
+            // y--;
+            ans += a * y;
+        }
+        cout << ans << "\n";
+    }
 }
 
-/************************************************************
- *                      Main Function                      *
- ************************************************************/
-
-Depressed_C0der
+signed main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    tst
-    Beche_achi();
+    int tc = 1;
+    cin >> tc;
 
-    Goodbye
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
 }
