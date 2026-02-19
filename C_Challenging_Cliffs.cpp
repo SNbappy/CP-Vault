15349b26070b689d97e980e61d5b8ce27febde11
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-02-19 14:33:12 +0600

    Accepted

---
+++ C_Challenging_Cliffs.cpp
@@ -0,0 +1,83 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-02-19 14:29:04
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(all(a));
+    if (n == 2)
+    {
+        cout << a[0] << " " << a[1] << "\n";
+        return;
+    }
+    int x = -1, mn = LLONG_MAX;
+    for (int i = 1; i < n; i++)
+    {
+        if (mn > abs(a[i] - a[i - 1]))
+        {
+            x = i;
+            mn = abs(a[i] - a[i - 1]);
+        }
+    }
+
+    for (int i = x; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    for (int i = 0; i < x; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
