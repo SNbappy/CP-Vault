3180f10b9a3fe2233ab7cfaac5fe4ebf1ab8574d
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-02-21 10:13:39 +0600

    Accepted

---
+++ B_Balls_of_Steel.cpp
@@ -0,0 +1,69 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-02-21 09:55:17
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].first >> a[i].second;
+    }
+    int ans = -1;
+    for (int i = 0; i < n; i++)
+    {
+        int mx = 0;
+        for (int j = 0; j < n; j++)
+        {
+            mx = max(mx, abs(a[i].first - a[j].first) + abs(a[j].second - a[i].second));
+        }
+        if(mx <= k)
+            ans = 1;
+    }
+    cout << ans << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
