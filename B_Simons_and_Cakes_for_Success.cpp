d700b2e3d6e9c7ddf9599670aee69d7fd8c5f5cf
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-02-27 12:19:08 +0600

    Accepted

---
+++ B_Simons_and_Cakes_for_Success.cpp
@@ -0,0 +1,64 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-02-26 20:46:59
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n;
+    cin >> n;
+    int x = n, ans = 1;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (x % i == 0) {
+            ans *= i;
+            while (x % i == 0)
+                x /= i;
+        }
+    }
+    if (x != 1)
+        ans *= x;
+    cout << ans << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
