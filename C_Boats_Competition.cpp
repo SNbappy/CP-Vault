1c0d1bb1c2b5f596815cb88b9546f1c2002db023
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-03-04 14:15:31 +0600

    Accepted

---
+++ C_Boats_Competition.cpp
@@ -0,0 +1,74 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-03-03 21:46:59
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n;
+    cin >> n;
+    vector<int> cnt(n + 1);
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        cnt[x]++;
+    }
+    int ans = 0;
+    for (int s = 2; s <= 2*n; s++)
+    {
+        int cur = 0;
+        for (int i = 0; i < (s + 1) / 2; i++)
+        {
+            if (s - i > n)
+            continue;
+            cur += min(cnt[i], cnt[s - i]);
+        }
+        if (s % 2 == 0)
+            cur += cnt[s / 2] / 2;
+        ans = max(ans, cur);
+    }
+    cout << ans << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
