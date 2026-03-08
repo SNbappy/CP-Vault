830166a21eca221d896a2facd5654e091a054bbe
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-03-08 13:21:59 +0600

    Accepted

---
+++ E_Monsters_easy_version.cpp
@@ -0,0 +1,75 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-03-08 12:04:46
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(all(a));
+    int cnt = 1;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // cout << a[i] << " " << cnt << '\n';
+        if (a[i] > cnt)
+        {
+            ans += a[i] - cnt;
+            cnt++;
+        }
+        else
+        {
+            cnt = a[i];
+            ++cnt;
+        }
+    }
+    cout << ans << '\n';
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
