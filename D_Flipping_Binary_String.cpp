670e3bffaee2a9fb980ff894b82853f783abadde
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-03-08 12:02:56 +0600

    Accepted

---
+++ D_Flipping_Binary_String.cpp
@@ -0,0 +1,94 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-03-07 19:36:48
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int cnt = 0;
+    int zero = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+            cnt++;
+        else
+            zero++;
+    }
+
+    if (zero % 2 == 1)
+    {
+        cout << zero << '\n';
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '0')
+                cout << i + 1 << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    if (cnt % 2 == 0)
+    {
+        cout << cnt << '\n';
+        if (cnt != 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == '1')
+                    cout << i + 1 << ' ';
+            }
+            cout << '\n';
+        }
+        return;
+    }
+    cout << -1 << '\n';
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
