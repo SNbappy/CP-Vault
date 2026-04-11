378224de40448ef208729848bcbc414f8b1ce4a7
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-04-11 22:43:20 +0600

    Accepted

---
+++ C_1_Equal_Multisets_Easy_Version.cpp
@@ -0,0 +1,94 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-04-11 22:20:19
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n), b(n);
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+        if (b[i] != -1)
+            mp[b[i]]++;
+    }
+    for (auto [x, y] : mp)
+    {
+        if (y > 1)
+        {
+            cout << "NO" << '\n';
+            return;
+        }
+    }
+    if (n == k)
+    {
+        cout << "YES" << '\n';
+        return;
+    }
+    for (int i = 0; i < n - k; i++)
+    {
+        if (a[i] != b[i] and b[i] != -1)
+        {
+            cout << "NO" << '\n';
+            return;
+        }
+    }
+    for (int i = k; i < n; i++)
+    {
+        if (a[i] != b[i] and b[i] != -1)
+        {
+            cout << "NO" << '\n';
+            return;
+        }
+    }
+    cout << "YES" << '\n';
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
