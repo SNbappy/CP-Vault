fa94b63818dc18d9837ab32e4a93c782fe82a68f
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-04-22 12:55:20 +0600

    Accepted

--- C_Simons_and_Posting_Blogs.cpp
+++ C_Simons_and_Posting_Blogs.cpp
@@ -1,7 +1,7 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
 Author: Depressed_C0der
-Created: 2026-04-17 23:21:30
+Created: 2026-04-22 11:39:49
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -30,7 +30,53 @@ using namespace std;
 
 void Depressed_C0der()
 {
+    int n;
+    cin >> n;
+    set<vector<int>> arr;
+    while (n--) {
+        int x;
+        cin >> x;
+        vector<int> nibo;
+        set<int> s;
+        while(x--) {
+            int xx;
+            cin >> xx;
+            if (!s.count(xx)) {
+                s.insert(xx);
+                nibo.push_back(xx);
+            }
+        }
+        reverse(all(nibo));
+        arr.insert(nibo);
+    }
+
+    vector<int> ans;
+
+    while (!arr.empty()) {
+        vector<int> nibo = *arr.begin();
+        arr.erase(arr.begin());
+
+        for (auto x: nibo)
+            ans.push_back(x);
+
+        auto x = arr;
+        set<int> temp(all(nibo));
+        arr.clear();
+
+        for (auto xx: x) {
+            vector<int> v;
+            
+            for (auto xxx: xx) {
+                if (!temp.count(xxx))
+                    v.push_back(xxx);
+            }
+            arr.insert(v);
+        }
+    }
     
+    for (auto x: ans)
+        cout << x << " ";
+    cout << "\n";
 }
 
 signed main()
