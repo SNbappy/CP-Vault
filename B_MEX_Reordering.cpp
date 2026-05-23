e76f39b220e297a268ebaeadf305777a30279986
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-05-23 14:21:07 +0600

    Accepted

---
+++ B_MEX_Reordering.cpp
@@ -0,0 +1,88 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-05-23 12:08:34
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(all(a));
+    map<int, int> mxl, mxr;
+
+    set<int> st;
+    for (int i = 0; i <= n; i++) {
+        st.insert(i);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        st.erase(a[i]);
+        mxl[i] = *st.begin();
+    }
+
+    st.clear();
+
+    for (int i = 0; i <= n; i++)
+    {
+        st.insert(i);
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        st.erase(a[i]);
+        mxr[i] = *st.begin();
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        if (mxl[i] == mxr[i + 1]){
+            cout << "NO" << "\n";
+            return;
+        }
+    }
+    cout << "YES" << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
