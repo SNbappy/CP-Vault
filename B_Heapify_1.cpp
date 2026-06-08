299da8cc9fdfed10566f4efc79112f389fb04308
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-06-08 14:05:15 +0600

    Accepted

---
+++ B_Heapify_1.cpp
@@ -0,0 +1,79 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-06-05 09:00:53
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    unordered_map<int, vector<int>> have, need;
+
+    for (int i = 1; i <= n; i++) {
+        int root = i;
+        while(root % 2 == 0)
+            root /= 2;
+
+        have[root].push_back(a[i]);
+        need[root].push_back(i);
+    }
+
+    for (auto [x, y]: have){
+        auto target = need[x];
+
+        sort(all(y));
+
+        if (target != y) {
+            cout << "NO" << '\n';
+            return;
+        }
+    }
+
+    cout << "YES" << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
