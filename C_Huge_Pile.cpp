40d1060b469167f9afc2ccd276fda1d1fd5b59a9
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-06-10 12:03:56 +0600

    Accepted

---
+++ C_Huge_Pile.cpp
@@ -0,0 +1,65 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-06-10 11:12:56
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n, k;
+    cin >> n >> k;
+    int l = n, r = n;
+    int cnt = 0;
+    while(r != 1) {
+        if (l <= k and k <= r)
+            break;
+        l = l / 2;
+        r = r / 2 + r % 2;
+        cnt++;
+    }
+    if (l == k || r == k)
+        cout << cnt << "\n";
+    else
+        cout << -1 << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
