03118e05f9cc91ba81967a01e3027290f38f56a9
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-09-11 18:15:29 +0600

    Accepted

--- Trailing_Zeroes_I.cpp
+++ Trailing_Zeroes_I.cpp
@@ -1,7 +1,7 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
 Author: Depressed_C0der
-Created: 2026-09-11 16:06:10
+Created: 2026-09-11 18:04:22
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -28,11 +28,58 @@ using namespace std;
 #define debug(...)
 #endif
 
+const int MAXN = 1e6;
+vector<bool> isPrime(MAXN + 1, true);
+vector<int> prime;
+
+void Sieve()
+{
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= MAXN; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= MAXN; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i <= MAXN; i++)
+    {
+        if (isPrime[i])
+            prime.push_back(i);
+    }
+}
+
 void Depressed_C0der()
 {
     int n;
     cin >> n;
-    
+
+    int ans = 1;
+
+    for (auto x : prime)
+    {
+        if (x * x > n)
+            break;
+        if (n % x == 0)
+        {
+            int pow = 0;
+            while (n % x == 0)
+            {
+                pow++;
+                n /= x;
+            }
+            ans *= (pow + 1);
+        }
+    }
+
+    if (n > 1)
+        ans *= 2;
+
+    cout << ans - 1 << "\n";
 }
 
 signed main()
@@ -40,7 +87,7 @@ signed main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-
+    Sieve();
     int tc = 1;
     cin >> tc;
 
