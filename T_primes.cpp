df6bbec995d55af95d9a1fffc60025d53aecaa68
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-09-12 03:45:12 +0600

    Accepted

--- T_primes.cpp
+++ T_primes.cpp
@@ -1,13 +1,25 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
 Author: Depressed_C0der
-Created: 2025-09-24 18:28:58
+Created: 2026-09-12 03:44:58
 */
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
 #define all(n) n.begin(), n.end()
-
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
 #ifndef ONLINE_JUDGE
 #define debug(...)                                                  \
     cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
@@ -16,47 +28,24 @@ using namespace std;
 #define debug(...)
 #endif
 
-const int N = 1e6+9;
-bool isPrime[N];
-
-void sieve(){
-    isPrime[1] = false;
-    for (int i = 2; i < N; i++)
-    {
-        isPrime[i] = true;
-    }
-    
-    for (int i = 2; i < N; i++)
-    {
-        if(isPrime[i]){
-            for (int j = i+i; j < N; j+=i)
-            {
-                isPrime[j] = false;
-            }            
-        }
-    }
+void Depressed_C0der()
+{
     
 }
-
 
 signed main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    sieve();
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
     {
-        int x;
-        cin >> x;
-        int y = sqrtl(x);
-        if(y * y == x and isPrime[y])
-            cout << "YES" << '\n';
-        else
-            cout << "NO" << '\n';
+        // cout << "Case " << i << ": ";
+        Depressed_C0der();
     }
     return 0;
 }
