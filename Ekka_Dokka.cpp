207626b5732de76c185176f3be33dabceb3db4b1
Md. Sabbir Hossain Bappy <bappy.just@gmail.com>
2026-09-13 16:29:05 +0600

    Accepted

---
+++ Ekka_Dokka.cpp
@@ -0,0 +1,63 @@
+/*
+بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+Author: Depressed_C0der
+Created: 2026-09-13 16:11:22
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define mii map<int, int>
+#define vpii vector<pair<int, int>>
+#ifndef ONLINE_JUDGE
+#define debug(...)                                                  \
+    cerr << "Line:" << __LINE__ << " [" << #__VA_ARGS__ << "] = ["; \
+    _print(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+void Depressed_C0der()
+{
+    int n;
+    cin >> n;
+    if (n % 2)
+    {
+        cout << "Impossible" << "\n";
+        return;
+    }
+    int odd = n, even = 1;
+    while (odd % 2 == 0) {
+        odd /= 2;
+        even *= 2;
+    }
+    cout << odd << " " << even << "\n";
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+    {
+        cout << "Case " << i << ": ";
+        Depressed_C0der();
+    }
+    return 0;
+}
